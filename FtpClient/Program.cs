526ffd53ce8d76fe0716507cf17d8b415a430137
Kristian Toustrup Langborg-Hansen <ktlh@smartlearning.dk>
2022-01-18 11:07:30 +0100

    Add project files.

---
+++ FtpClient/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+
+FtpWebRequest request = (FtpWebRequest)WebRequest.Create("ftp://inverter.westeurope.cloudapp.azure.com");
+request.Method = WebRequestMethods.Ftp.ListDirectory;
+
+Console.WriteLine("Indtast brugernavn");
+string user = Console.ReadLine();
+
+Console.WriteLine("Indtast adgangskode");
+string password = Console.ReadLine();
+
+request.Credentials = new NetworkCredential(user, password);
+
+FtpWebResponse response = (FtpWebResponse)request.GetResponse();
+var stream = response.GetResponseStream();
+var reader = new StreamReader(stream);
+
+Console.WriteLine(reader.ReadToEnd());
+
+reader.Close();
+response.Close();
